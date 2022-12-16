88e038ced44143ce0373326f303a7f7cf6994594
suraj <suraj@gmail.com>
2022-12-16 16:48:59 +0530

    [Add]:intial commit

---
+++ gift_shop/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from gift_shop.users.models import User
+from gift_shop.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
