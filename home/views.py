e8789cb1196b3200c92b16094cf916ace517e068
Suraj Patidar <suraj.pysquad@gmail.com>
2023-01-03 08:15:38 +0530

    [Refactor]: add carousel for categories

--- home/views.py
+++ home/views.py
@@ -12,6 +12,7 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context=super().get_context_data(**kwargs)
+        context['pro']='product'
         context['sliders'] = Slider.objects.all()
         context['products'] = Product.objects.all()
         context['categorys'] = Category.objects.all()
